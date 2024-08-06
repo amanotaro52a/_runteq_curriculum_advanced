49295afcbd01d0cc43e95e2ead9ee40394ad8351
天野太郎 <amanotaro@amanotarounoMacBook-Air.local>
2024-08-06 14:01:32 +0900

    Rails応用 6 ver4.0

--- app/policies/tag_policy.rb
+++ app/policies/tag_policy.rb
@@ -1,17 +1,17 @@
 class TagPolicy < TaxonomyPolicy
   def index?
-      user.admin? || user.editor?
+    user.admin? || user.editor?
   end
 
   def create?
-      user.admin? || user.editor?
+    user.admin? || user.editor?
   end
 
   def update?
-      user.admin? || user.editor?
+    user.admin? || user.editor?
   end
 
   def destroy?
-      user.admin? || user.editor?
+    user.admin? || user.editor?
   end
 end
