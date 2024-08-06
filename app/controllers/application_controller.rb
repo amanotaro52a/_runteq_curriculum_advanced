e9eb163edd73b1d3899a60a1b62e8fc9f5b4a707
天野太郎 <amanotaro@amanotarounoMacBook-Air.local>
2024-08-06 14:29:50 +0900

    Rails応用 6 ver5.0

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -58,6 +58,6 @@ class ApplicationController < ActionController::Base
   end
 
   def user_not_authorized
-    render file: Rails.public_path('403.html'), status: :forbidden
+    render file: Rails.root.join('public', '403.html'), status: :forbidden
   end
 end
