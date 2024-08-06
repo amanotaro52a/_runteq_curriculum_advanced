fa4b4c17681249d9468f2c3861d7a4cd2c5a1583
天野太郎 <amanotaro@amanotarounoMacBook-Air.local>
2024-08-06 11:52:26 +0900

    Rails応用 6 ver2.0

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   before_action :require_login
   before_action :current_site
@@ -54,5 +55,9 @@ class ApplicationController < ActionController::Base
       new_arrivals: true,
       categories: true
     }
+  end
+
+  def user_not_authorized
+    render file: "#{Rails.root}/public/403.html", status: :forbidden
   end
 end
--- app/policies/category_policy.rb
+++ app/policies/category_policy.rb
@@ -1,2 +1,17 @@
 class CategoryPolicy < TaxonomyPolicy
+  def index?
+      user.admin? || user.editor?
+  end
+
+  def create?
+      user.admin? || user.editor?
+  end
+
+  def update?
+      user.admin? || user.editor?
+  end
+
+  def destroy?
+      user.admin? || user.editor?
+  end
 end
